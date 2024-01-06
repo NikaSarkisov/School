93b15c66053f05270866ccba19df15263ab0cce7
Alex Sarkisov <alexsa@matrix.co.il>
2024-01-06 21:18:27 +0200

    -

---
+++ ConsoleApps/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Please enter a number between 1 to 9");
+string input = Console.ReadLine();
+
+int num = int.Parse(input);
+
+for (int y = 0; y < num; y++)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write(i+1 + " ");
+    }
+    Console.WriteLine();
+}
+
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write(i + 1 + " ");
+    }
---
+++ Exercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Please enter a number between 1 to 9");
+string input = Console.ReadLine();
+
+int num = int.Parse(input);
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write(i + " ");
+}
