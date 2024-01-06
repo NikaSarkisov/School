f763349461de3e29dea6462e8442b88df205d45b
Alex Sarkisov <alexsa@matrix.co.il>
2024-01-06 22:31:50 +0200

    -

--- ConsoleApps/ConsoleApp1/Program.cs
+++ ConsoleApps/ConsoleApp1/Program.cs
@@ -4,16 +4,63 @@ string input = Console.ReadLine();
 
 int num = int.Parse(input);
 
-for (int y = 0; y < num; y++)
+for (int y = 1; y <= num; y++)
 {
-    for (int i = 0; i < num; i++)
+    for (int i = 1; i <= num; i++)
     {
-        Console.Write(i+1 + " ");
+        Console.Write(i + " ");
     }
     Console.WriteLine();
 }
+Console.WriteLine();
 
-    for (int i = 0; i < num; i++)
+for (int y = 1; y <= num; y++)
+{
+    for (int i = num; i >= 1; i--)
     {
-        Console.Write(i + 1 + " ");
+        Console.Write(i + " ");
     }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int y = 1; y <= num; y++)
+{
+    for (int i = 1; i <= y; i++)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int y = 1; y <= num; y++)
+{
+    for (int i = y; i >= 1; i--)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int y = 1; y <= num; y++)
+{
+    if (y%2==1)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.Write(i + " ");
+        }
+    }
+    else
+    {
+        for (int i = num; i >= 1; i--)
+        {
+            Console.Write(i + " ");
+        }
+    }
+
+    Console.WriteLine();
+
+}
